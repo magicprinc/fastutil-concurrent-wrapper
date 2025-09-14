87308932fb7ec3357f8d2a98b7eb5e3f64a55dc3
A.Fink <aprpda@gmail.com>
2025-09-14 18:44:29 +0300

    move jmh to subproject to simplify build

--- src/main/java/com/trivago/fastutilconcurrentwrapper/PrimitiveMapBuilder.java
+++ src/main/java/com/trivago/fastutilconcurrentwrapper/PrimitiveMapBuilder.java
@@ -1,51 +1,51 @@
 package com.trivago.fastutilconcurrentwrapper;
 
 public abstract class PrimitiveMapBuilder<T extends PrimitiveKeyMap,V> {
-    protected MapMode mapMode = MapMode.BUSY_WAITING;
-    protected int buckets = 8;
-    protected int initialCapacity = 100_000;
-    protected float loadFactor = 0.8f;
-    protected V defaultValue;
+	protected MapMode mapMode = MapMode.BUSY_WAITING;
+	protected int buckets = 8;
+	protected int initialCapacity = 100_000;
+	protected float loadFactor = 0.8f;
+	protected V defaultValue;
 
-    protected PrimitiveMapBuilder () {
-    }
+	protected PrimitiveMapBuilder () {
+	}
 
-    public final PrimitiveMapBuilder<T,V> withBuckets (int buckets) {
-        this.buckets = buckets;
-        return this;
-    }
+	public final PrimitiveMapBuilder<T,V> withBuckets (int buckets) {
+		this.buckets = buckets;
+		return this;
+	}
 
-    public final PrimitiveMapBuilder<T,V> withInitialCapacity (int initialCapacity) {
-        this.initialCapacity = initialCapacity;
-        return this;
-    }
+	public final PrimitiveMapBuilder<T,V> withInitialCapacity (int initialCapacity) {
+		this.initialCapacity = initialCapacity;
+		return this;
+	}
 
-    public final PrimitiveMapBuilder<T,V> withLoadFactor (float loadFactor) {
-        this.loadFactor = loadFactor;
-        return this;
-    }
+	public final PrimitiveMapBuilder<T,V> withLoadFactor (float loadFactor) {
+		this.loadFactor = loadFactor;
+		return this;
+	}
 
-    public final PrimitiveMapBuilder<T,V> withMode (MapMode mapMode) {
-        this.mapMode = mapMode;
-        return this;
-    }
+	public final PrimitiveMapBuilder<T,V> withMode (MapMode mapMode) {
+		this.mapMode = mapMode;
+		return this;
+	}
 
-    public final PrimitiveMapBuilder<T,V> withDefaultValue (V defaultValue) {
-        this.defaultValue = defaultValue;
-        return this;
-    }
+	public final PrimitiveMapBuilder<T,V> withDefaultValue (V defaultValue) {
+		this.defaultValue = defaultValue;
+		return this;
+	}
 
-    public abstract T build ();
+	public abstract T build ();
 
-    public enum MapMode {
-        BUSY_WAITING,
-        BLOCKING
-    }
+	public enum MapMode {
+		BUSY_WAITING,
+		BLOCKING
+	}
 
-    @Override
-    public String toString () {
-        return "PrimitiveMapBuilder{mapMode=%s, buckets=%d, initialCapacity=%d, loadFactor=%s, def=%s}".formatted(
-            mapMode, buckets, initialCapacity, loadFactor, defaultValue
-        );
-    }
+	@Override
+	public String toString () {
+		return "PrimitiveMapBuilder{mapMode=%s, buckets=%d, initialCapacity=%d, loadFactor=%s, def=%s}".formatted(
+			mapMode, buckets, initialCapacity, loadFactor, defaultValue
+		);
+	}
 }
