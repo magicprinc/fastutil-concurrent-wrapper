c11ca37d8b39b3134e48e21dd6637ec740231881
A.Fink <aprpda@gmail.com>
2025-09-15 17:34:18 +0300

    better CFUtil.bucket

--- src/main/java/com/trivago/fastutilconcurrentwrapper/util/CFUtil.java
+++ src/main/java/com/trivago/fastutilconcurrentwrapper/util/CFUtil.java
@@ -2,42 +2,65 @@ package com.trivago.fastutilconcurrentwrapper.util;
 
 import it.unimi.dsi.fastutil.HashCommon;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 
 /**
  Concurrent Fast Util
- @see  */
+
+ @see it.unimi.dsi.fastutil.HashCommon#mix
+ @see it.unimi.dsi.fastutil.HashCommon#murmurHash3
+ @see java.util.Objects#hashCode(Object)
+ @see jdk.internal.util.random.RandomSupport#mixMurmur64
+ @see org.springframework.util.ConcurrentReferenceHashMap#getHash(Object)
+*/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CFUtil {
 	public static final StackTraceElement[] EMPTY_STACK_TRACE = new StackTraceElement[0];
 
-	/** @see #bucket */
-	public static int bucket (int hashOrKey, @Positive int bucketSize) {
-		return Math.abs(HashCommon.mix(hashOrKey) % bucketSize);
+	/** @see it.unimi.dsi.fastutil.HashCommon#murmurHash3 */
+	public static int hash (int hash) {
+		return HashCommon.murmurHash3(hash);
 	}
 
 	/** @see #bucket */
-	public static int bucket (@Nullable Object object4hashCode, @Positive int bucketSize) {
-		return object4hashCode != null ? bucket(object4hashCode.hashCode(), bucketSize)
-			: 0;
+	public static @PositiveOrZero int bucket (int hashOrKey, @Positive int bucketSize) {
+		int goodHash = HashCommon.murmurHash3(hashOrKey);
+		return Math.abs(goodHash % bucketSize);
 	}
 
 	/**
-	 Get positive, quite `random` bucket index between 0 and bucketSize-1 for any key
+	 @see #bucket
+	 @see java.util.Objects#hashCode(Object)
+	 */
+	public static @PositiveOrZero int bucket (@Nullable Object object4hashCode, @Positive int bucketSize) {
+		if (object4hashCode == null){
+			return 0;
+		} else if (object4hashCode instanceof Long n){
+			return bucket(n.longValue(), bucketSize);
+		} else {
+			return bucket(object4hashCode.hashCode(), bucketSize);
+		}
+	}
+
+	/**
+	 Get positive, quite `random` bucket index between [0 and bucketSize-1] for any key.
 	 Fast. Safe for negative keys (including Long.MIN_VALUE, Integer.MIN_VALUE)
 
-	 FastUtil has ❌ HashCommon#mix(long), but we use ✅ Long.hashCode + mix(int) because with mix(long):
+	 FastUtil has ❌ HashCommon mix/murmurHash3(long), but we use ✅ Long.hashCode + murmurHash3(int) because (long):
 
 	 mix(1L) ≠ mix(1) → it is against common knowledge and expectations
 	 mix(1L) ≠ mix(Long.valueOf(1L)) → 😱
 
-	 @see it.unimi.dsi.fastutil.HashCommon#mix(long)
-	 @see it.unimi.dsi.fastutil.HashCommon#mix(int)
+	 @see #hash(int)
 	 @see Long#hashCode(long)
-	 */
-	public static int bucket (long hashOrKey, @Positive int bucketSize) {
-		//× return (int) Math.abs(HashCommon.mix(hashOrKey) % bucketSize);
-		int intHash = Long.hashCode(hashOrKey);
-		return Math.abs(HashCommon.mix(intHash) % bucketSize);
+	*/
+	public static @PositiveOrZero int bucket (long hashOrKey, @Positive int bucketSize) {
+		return hashOrKey > Integer.MAX_VALUE || hashOrKey < Integer.MIN_VALUE
+			? bucket(Long.hashCode(hashOrKey), bucketSize)
+			: bucket((int) hashOrKey, bucketSize);// long uses low 32bit only
 	}
 
 	/**
@@ -49,9 +72,7 @@ public final class CFUtil {
 	 * @param keyLo to make the lower bits
 	 * @return the compound key
 	 */
-	public static long compoundKey (int keyHi, final int keyLo) {
+	public static long compoundKey (int keyHi, int keyLo) {
 		return ((long)keyHi << 32) | (keyLo & 0xFfFf_FfFfL);
 	}
-
-	private CFUtil (){ throw new AssertionError(); }//new
 }
--- src/test/java/com/trivago/fastutilconcurrentwrapper/intkey/PrimitiveConcurrentMapTest.java
+++ src/test/java/com/trivago/fastutilconcurrentwrapper/intkey/PrimitiveConcurrentMapTest.java
@@ -2,69 +2,101 @@ package com.trivago.fastutilconcurrentwrapper.intkey;
 
 import com.trivago.fastutilconcurrentwrapper.PrimitiveConcurrentMap;
 import com.trivago.fastutilconcurrentwrapper.util.CFUtil;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- @see PrimitiveConcurrentMap */
+ @see PrimitiveConcurrentMap
+*/
 class PrimitiveConcurrentMapTest {
 	@Test
 	void _hashInt () {
-		assertEquals(50_880, CFUtil.bucket(Integer.MIN_VALUE, 100_000));
-		assertEquals(47_470, CFUtil.bucket(-Integer.MAX_VALUE, 100_000));
-		assertEquals(74_300, CFUtil.bucket(-100_000, 100_000));
-		assertEquals(56_431, CFUtil.bucket(-1, 100_000));
+		assertEquals(74720, CFUtil.bucket(Integer.MIN_VALUE, 100_000));
+		assertEquals(79862, CFUtil.bucket(-Integer.MAX_VALUE, 100_000));
+		assertEquals(3539, CFUtil.bucket(-100_000, 100_000));
+		assertEquals(57924, CFUtil.bucket(-2, 100_000));
+		assertEquals(83783, CFUtil.bucket(-1, 100_000));
 		assertEquals(0, CFUtil.bucket(0, 100_000));
-		assertEquals(3_410, CFUtil.bucket(1, 100_000));
-		assertEquals(82_299, CFUtil.bucket(100_000, 100_000));
-		assertEquals(59_985, CFUtil.bucket(Integer.MAX_VALUE, 100_000));
+		assertEquals(76727, CFUtil.bucket(1, 100_000));
+		assertEquals(47078, CFUtil.bucket(2, 100_000));
+		assertEquals(55934, CFUtil.bucket(100_000, 100_000));
+		assertEquals(67416, CFUtil.bucket(Integer.MAX_VALUE, 100_000));
 	}
 
 	@Test
 	void _hashLong () {
-		assertEquals(50_880, CFUtil.bucket(Long.MIN_VALUE, 100_000));
-		assertEquals(50_880, CFUtil.bucket(Integer.MIN_VALUE & 0xFFFFFFFFL, 100_000));
-		assertEquals(59_985, CFUtil.bucket((long) Integer.MIN_VALUE, 100_000));
-		assertEquals(47_470, CFUtil.bucket(-Integer.MAX_VALUE & 0xFFFFFFFFL, 100_000));
-		assertEquals(96_447, CFUtil.bucket((long) -Integer.MAX_VALUE, 100_000));
-		assertEquals(16_093, CFUtil.bucket(-100_000L, 100_000));
-		assertEquals(0, CFUtil.bucket(-1L, 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.MIN_VALUE, 100_000));
+		assertEquals(79862, CFUtil.bucket(Long.MIN_VALUE+1, 100_000));
+		assertEquals(74720, CFUtil.bucket(Integer.MIN_VALUE & 0xFFFFFFFFL, 100_000));
+		assertEquals(74720, CFUtil.bucket((long) Integer.MIN_VALUE, 100_000));
+		assertEquals(79862, CFUtil.bucket(-Integer.MAX_VALUE & 0xFFFFFFFFL, 100_000));
+		assertEquals(79862, CFUtil.bucket((long) -Integer.MAX_VALUE, 100_000));
+		assertEquals(3539, CFUtil.bucket(-100_000L, 100_000));
+		assertEquals(83783, CFUtil.bucket(-1L, 100_000));
 		assertEquals(0, CFUtil.bucket(0L, 100_000));
-		assertEquals(3_410, CFUtil.bucket(1L, 100_000));
-		assertEquals(82_299, CFUtil.bucket(100_000L, 100_000));
-		assertEquals(59_985, CFUtil.bucket((long) Integer.MAX_VALUE, 100_000));
-		assertEquals(50_880, CFUtil.bucket(Long.MAX_VALUE, 100_000));
+		assertEquals(76727, CFUtil.bucket(1L, 100_000));
+		assertEquals(55934, CFUtil.bucket(100_000L, 100_000));
+		assertEquals(67416, CFUtil.bucket((long) Integer.MAX_VALUE, 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.MAX_VALUE, 100_000));
 	}
 
-	@Test  @Disabled("Total checks: 38510819404, time: 8551,564, op/sec: 189_126_936")
+	@Test
 	void longsAreSame () {
 		long total = 0, t = System.nanoTime();
-		for (long i = Long.MIN_VALUE, max = Long.MAX_VALUE - 479001599-9; i < max; i += 479001599){
-			assertEquals(CFUtil.bucket(Long.valueOf(i), 1_000_000), CFUtil.bucket(i, 1_000_000));
+		for (long i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE; i++){
+			assertEquals(CFUtil.bucket(Long.valueOf(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			assertEquals(CFUtil.bucket((int)i, 4_000_000), CFUtil.bucket(i, 4_000_000));
 			total++;
 		}
 		t = System.nanoTime() - t;
-		System.out.printf("\nTotal checks: %d, time: %.3f, op/sec: %d\n\n", total, t / 1_000 / 1000.0, total*1_000_000_000/t);
+		System.out.printf("\nTotal checks (inside int): %d, time: %.3f, op/sec: %d\n\n", total, t / 1_000 / 1000.0, total*1_000_000_000/t);
+
+		long x = Long.MIN_VALUE;
+		assertEquals(CFUtil.bucket(Long.valueOf(x), 4_000_000), CFUtil.bucket(x, 4_000_000));
+		x = Long.MAX_VALUE;
+		assertEquals(CFUtil.bucket(Long.valueOf(x), 4_000_000), CFUtil.bucket(x, 4_000_000));
+		assertEquals(CFUtil.bucket(Long.hashCode(x), 4_000_000), CFUtil.bucket(x, 4_000_000));
+
+		total = 0;
+		var r = ThreadLocalRandom.current();
+		for (int z = 0; z < 4_000_000; z++){
+			long i = r.nextLong();
+			assertEquals(CFUtil.bucket(Long.valueOf(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			i = Long.MIN_VALUE + z;
+			assertEquals(CFUtil.bucket(Long.valueOf(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			assertEquals(CFUtil.bucket(Long.hashCode(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			i = Long.MAX_VALUE - z;
+			assertEquals(CFUtil.bucket(Long.valueOf(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			assertEquals(CFUtil.bucket(Long.hashCode(i), 4_000_000), CFUtil.bucket(i, 4_000_000));
+			total++;
+		}
+		assertEquals(4_000_000, total);
 	}
 
 	@Test
 	void _hashObj () {
 		assertEquals(0, CFUtil.bucket(null, 100_000));
 
-		assertEquals(50_880, CFUtil.bucket(Long.valueOf(Long.MIN_VALUE), 100_000));
-		assertEquals(50_880, CFUtil.bucket(Long.valueOf(Integer.MIN_VALUE & 0xFFFFFFFFL), 100_000));
-		assertEquals(59_985, CFUtil.bucket(Long.valueOf(Integer.MIN_VALUE), 100_000));
-		assertEquals(47_470, CFUtil.bucket(Long.valueOf(-Integer.MAX_VALUE & 0xFFFFFFFFL), 100_000));
-		assertEquals(96_447, CFUtil.bucket(Long.valueOf(-Integer.MAX_VALUE), 100_000));
-		assertEquals(16_093, CFUtil.bucket(Long.valueOf(-100_000L), 100_000));
-		assertEquals(0, CFUtil.bucket(Long.valueOf(-1L), 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.valueOf(Long.MIN_VALUE), 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.valueOf(Integer.MIN_VALUE & 0xFFFFFFFFL), 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.valueOf(Integer.MIN_VALUE), 100_000));
+
+		assertEquals(79862, CFUtil.bucket(Long.valueOf(-Integer.MAX_VALUE & 0xFFFFFFFFL), 100_000));
+		assertEquals(79862, CFUtil.bucket(Long.valueOf(-Integer.MAX_VALUE), 100_000));
+		assertEquals(3539, CFUtil.bucket(Long.valueOf(-100_000L), 100_000));
+		assertEquals(57924, CFUtil.bucket(Long.valueOf(-2L), 100_000));
+		assertEquals(57924, CFUtil.bucket(Integer.valueOf(-2), 100_000));
+		assertEquals(83783, CFUtil.bucket(Long.valueOf(-1L), 100_000));
 		assertEquals(0, CFUtil.bucket(Long.valueOf(0L), 100_000));
-		assertEquals(3_410, CFUtil.bucket(Long.valueOf(1L), 100_000));
-		assertEquals(82_299, CFUtil.bucket(Long.valueOf(100_000L), 100_000));
-		assertEquals(59_985, CFUtil.bucket(Long.valueOf(Integer.MAX_VALUE), 100_000));
-		assertEquals(50_880, CFUtil.bucket(Long.valueOf(Long.MAX_VALUE), 100_000));
+		assertEquals(76727, CFUtil.bucket(Long.valueOf(1L), 100_000));
+		assertEquals(47078, CFUtil.bucket(Long.valueOf(2L), 100_000));
+		assertEquals(47078, CFUtil.bucket(Integer.valueOf(2), 100_000));
+		assertEquals(55934, CFUtil.bucket(Long.valueOf(100_000L), 100_000));
+		assertEquals(67416, CFUtil.bucket(Long.valueOf(Integer.MAX_VALUE), 100_000));
+		assertEquals(74720, CFUtil.bucket(Long.valueOf(Long.MAX_VALUE), 100_000));
 	}
 
 	@Test
